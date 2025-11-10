50421d98de7beade6ad498d3257df8dfb1c0da5e
Soheil Assadi <suheyl.asadi@gmail.com>
2025-11-10 14:19:43 +0300

    add products to localStorage()

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,24 @@
-export let cart = [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+// Give a default value for now if the cart is null
+if (!cart) {
+  cart = [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+    },
+  ];
+}
+
+/* Adding selected items to localStorage() thus page refresh
+   does not affect products selected */
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 // function to use in amazon.js '.js-add-to-cart' element
 export function addToCart(productId) {
@@ -27,6 +38,8 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+
+  saveToStorage();
 }
 
 // function to remove product from the cart
@@ -39,4 +52,5 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+  saveToStorage();
 }
