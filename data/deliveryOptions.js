2e6f07e424409af4e9c8184cb7cd779dd11cd99a
Soheil Assadi <suheyl.asadi@gmail.com>
2025-11-11 22:47:00 +0300

    dataset connect two parts in checkout page

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -5,12 +5,12 @@ export const deliveryOptions = [
     priceCents: 0,
   },
   {
-    id: 2,
+    id: '2',
     deliveryDays: 3,
     priceCents: 499,
   },
   {
-    id: 3,
+    id: '3',
     deliveryDays: 1,
     priceCents: 999,
   },
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,8 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions } from '../data/deliveryOptions.js';
-
-// ...existing code...
 
 let cartSummaryHTML = '';
 
@@ -79,7 +77,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
             <input
                 type="radio"
                 id="delivery-${matchingProduct.id}-${deliveryOption.id}"
@@ -155,5 +155,12 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
       `.js-cart-item-container-${productId}`
     );
     if (container) container.remove();
+  });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
